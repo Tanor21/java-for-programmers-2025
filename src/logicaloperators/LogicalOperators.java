be198684f7ade8a09d4a27c32f24e3551dfca03c
Tanor <cisseousmanetanor@gmail.com>
2025-07-30 16:22:37 +0100

    feat: add logical operations and average calculation examples

---
+++ src/logicaloperators/LogicalOperators.java
@@ -0,0 +1,17 @@
+package logicaloperators;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        System.out.println("Learn logical operators in java");
+
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 9;
+
+        boolean result = x > y && a > b;
+
+        System.out.println(result);
+
+    }
+}
